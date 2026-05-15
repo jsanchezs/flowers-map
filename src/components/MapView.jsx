a93020ab6993efa1dcab2f52486f1c3114e6e635
jsanchezs <jesus.sanchez.santamaria@gmail.com>
2026-05-15 16:51:45 +0200

    Switch map tiles to Carto

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -49,8 +49,8 @@ function MapView({ points, selectedPoint, selectedPointId, onSelectPoint }) {
         className="sound-map"
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.stadiamaps.com/">Stadia Maps</a> &copy; <a href="https://www.stamen.com/">Stamen Design</a> &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://carto.com/attributions">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         />
 
         <CenterOnPoint point={selectedPoint} />
